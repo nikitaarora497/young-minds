fb445e44167d86911155ca0f724b6d0f592f133b
unknown <sanchit@gmail.com>
2020-09-07 19:46:18 +0530

    young minds

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Logo from './logo.jpg';
+import User from './user.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h1>
+        Hello,<p>Udit Sharma</p> 
+        </h1>
+        <div><h6>0 followers|0 following</h6></div>
+    
+      <div className="img">
+        <img src={Logo} alt='website logo' />
+        </div>
+        <div className="img1">
+        <img src={User} alt='website logo' />
+        </div>
+      
+
+      </div>
+        );
+}
+function img() {
+  return (
+    <div>
+       <div className="img">
+        <img src={Logo} alt='website logo' />
+        </div>
+        <div className="img1">
+      </div>
+      
+
+      </div>
+        );
 }
 
 export default App;
+//export default img;
